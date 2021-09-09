c19787150924b7d1b4b34755a9816f83f6df1f7e
Victor Franco <vfranco.itc@gmail.com>
2021-09-08 19:41:45 -0500

    Added FB_in_string.py

---
+++ strings & arrays/FB_in_string.py
@@ -0,0 +1,24 @@
+
+# Create string with 'Facebook'
+# Create counter variable
+# iterate over characters of given word
+# if char in given word == char in facebook, add counter
+# if len == facebook, true, else: false  
+
+def fbInString(input):
+    fb = "facebook"
+    counter = 0
+
+    for i in range(len(input)):
+        if input[i] == fb[counter]:
+            counter += 1
+        
+        if counter == len(fb):
+            return True
+    
+    return False
+
+if __name__ == '__main__':
+    print(fbInString("ffffaaccccebbok"))
+    print(fbInString("is instagram owned by facebook?"))
+    print(fbInString("ffffaacccebbooook"))
