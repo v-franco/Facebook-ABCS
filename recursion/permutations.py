0eada289f0144b48262d02de859938fd4a59f846
Victor Franco <vfranco.itc@gmail.com>
2021-09-09 22:06:48 -0500

    Added recursion folder

---
+++ recursion/permutations.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'string_permutations' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY alphabet
+#  2. INTEGER permutation_length
+#
+
+def string_permutations(alphabet, permutation_length):
+    # Write your code here
+        
+
+if __name__ == '__main__':
+
+    alphabet_input = input().rstrip().split()
+
+    permutation_length_input = int(input().strip())
+
+    string_permutations(alphabet_input, permutation_length_input)
