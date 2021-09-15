2f03e294e17ebe6a9c36e2c6a11dcde3010247ab
Victor Franco <vfranco.itc@gmail.com>
2021-09-14 19:08:42 -0500

    Added session code cpp_stairs

--- session_exercises/cpp_stairs_attempt.cpp
+++ session_exercises/cpp_stairs_attempt.cpp
@@ -9,18 +9,21 @@ int steps[]={1,2,3};
 
 int climbHelper(int stairStart, int numStairs, int combinations){
   for(int i = 0; i < 3; i++){
-    if(stairStart + steps[i] <= numStairs){
-      climbHelper(stairStart + steps[i], numStairs - 1, combinations++);
-        } 
-        else{
-            return combinations;
-        }
+    if(stairStart + steps[i] < numStairs){
+      return combinations + climbHelper(stairStart + steps[i], numStairs, combinations);
+    } 
+    else if(stairStart + steps[i] == numStairs){
+      return 1;
+    } else{
+      return 0;
     }
+  }
 }
 
 int climb(int numStairs){
   int combinations = 0;
   int stairStart = 0;
+  int total = 0;
   
   if (numStairs == 1){
     combinations = 1;
@@ -28,16 +31,17 @@ int climb(int numStairs){
   }
   
   else{
-        for(int i = 0; i < 3; i++){
-            return climbHelper(stairStart + steps[i], numStairs - 1, combinations);
-        }
-  
+    for(int i = 0; i < 3; i++){
+      total += climbHelper(stairStart + steps[i], numStairs, combinations);
     }
+    return total;
+  }
+  
 }
 
 int main() {
   int numStairs = 0;
   cin >> numStairs;
-  climb(numStairs);
+  cout << climb(numStairs) << endl;
   return 0;
 }
